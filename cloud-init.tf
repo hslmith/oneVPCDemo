3511a6d9871311592ef637d74039d5df33e88bce
hslmith <kyrell@gmail.com>
2021-05-19 10:45:18 -0500

    first commit

---
+++ cloud-init.tf
@@ -0,0 +1,22 @@
+/*
+#################################################
+# Define Cloud-init scripts to run on provisioning
+#################################################
+
+
+data "local_file" "cloud-config-web-txt" {
+  filename        = "nginx.txt"
+}
+
+data "template_cloudinit_config" "cloud-init-config" {
+  gzip            = false
+  base64_encode   = false
+
+  part {
+    filename = "init.cfg"
+    content_type = "text/cloud-config"
+    content = "${data.local_file.cloud-config-web-txt.content}"
+  }
+}
+
+*/
